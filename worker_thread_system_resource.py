e00c95c82a5250177e2234da2238abc075940eef
itakurah <122729995+itakurah@users.noreply.github.com>
2023-04-13 23:18:38 +0200

    -update: renamed class

--- worker_thread_system_resource.py
+++ worker_thread_system_resource.py
@@ -1,19 +1,19 @@
 from PyQt5 import QtCore
 
 
-class WorkerThreadMemory(QtCore.QThread):
+class WorkerThreadSystemResource(QtCore.QThread):
     update_memory = QtCore.pyqtSignal()
 
     def __init__(self):
         # Use super() to call __init__() methods in the parent classes
-        super(WorkerThreadMemory, self).__init__()
+        super(WorkerThreadSystemResource, self).__init__()
         # The boolean variable to break the while loop in self.run() method
         self.running = True
 
     def run(self):
         while self.running:
             self.update_memory.emit()
-            self.msleep(500)
+            self.msleep(2000)
 
     def stop(self):
         # terminate the while loop in self.run() method
