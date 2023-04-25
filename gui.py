3186bf7145c750aa8f838e09646a83c6355a94fe
itakurah <122729995+itakurah@users.noreply.github.com>
2023-04-25 16:55:58 +0200

    -refactor: moved style definition to custom.css

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import QTimer
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QDateTime
 from PyQt5.QtWidgets import QComboBox, QLabel, QPushButton, QGroupBox, QRadioButton, QButtonGroup, QCheckBox, \
-    QStatusBar, QDesktopWidget, QColorDialog, QSlider
+    QStatusBar, QDesktopWidget, QColorDialog, QSlider, QWidget
 
 from camera_helper import get_connected_camera_alias
 from worker_thread_pause_screen import WorkerThreadPauseScreen
@@ -15,7 +15,7 @@ def load(self):
     """
     Loads the gui components
     """
-    # window settings
+    # window properties
     self.gui_width = 870
     self.gui_height = 540
     self.setWindowTitle("YOLO Sitting Posture Detector")
@@ -25,7 +25,7 @@ def load(self):
     # centers the window
     center_window(self)
 
-    # combobox settings
+    # combobox properties
     combobox_camera_list_width = 200
     combobox_camera_list_x = 20
     combobox_camera_list_y = 20
@@ -36,7 +36,7 @@ def load(self):
     self.combobox_camera_list.setFixedWidth(combobox_camera_list_width)
     self.combobox_camera_list.setFixedHeight(25)
 
-    # image label settings
+    # image label properties
     self.label_stream = QLabel(self)
     self.label_stream.setStyleSheet("border: 2px solid black")
     self.label_stream_width = 600
@@ -46,19 +46,25 @@ def load(self):
     self.label_stream.move(combobox_camera_list_x, combobox_camera_list_y + 30)
     self.label_stream.setHidden(True)
 
-    # image label settings
+    # image label properties
     self.label_no_camera = QLabel(self)
     self.label_no_camera.setStyleSheet("border: 0px solid black")
     self.label_no_camera.setFixedWidth(self.label_stream_width)
     self.label_no_camera.setFixedHeight(self.label_stream_height)
     self.label_no_camera.move(combobox_camera_list_x, combobox_camera_list_y + 30)
 
-    # start button settings
+    # start button properties
     button_start_width = 80
     button_start_height = 27
     button_start_x = combobox_camera_list_x + combobox_camera_list_width + 5
     button_start_y = 50
     self.button_start = QPushButton('Start', self)
+    self.button_start.setStyleSheet('QPushButton:enabled {'
+                                    'background-color: #4269b9;'
+                                    'border: 1px solid white;}'
+                                    'QPushButton:disabled,'
+                                    'QPushButton:disabled:hover {'
+                                    'background-color: #323844;}')
     self.button_start.setFixedHeight(button_start_height)
     self.button_start.setFixedWidth(button_start_width)
     self.button_start.move(button_start_x, combobox_camera_list_y - 1)
@@ -67,21 +73,24 @@ def load(self):
     button_stop_x = button_start_width + button_start_x + 5
     button_stop_y = 50
     self.button_stop = QPushButton('Stop', self)
+    self.button_stop.setStyleSheet('QPushButton:enabled {'
+                                   'background-color: #4269b9;'
+                                   'border: 1px solid white;}'
+                                   'QPushButton:disabled,'
+                                   'QPushButton:disabled:hover {'
+                                   'background-color: #323844;}')
     self.button_stop.setFixedHeight(27)
     self.button_stop.setFixedWidth(80)
     self.button_stop.move(button_stop_x, combobox_camera_list_y - 1)
 
-    # groupbox settings
+    # groupbox properties
     self.groupbox_frame_options = QGroupBox(self)
     self.groupbox_frame_options.setTitle('Frame Options')
     self.groupbox_frame_options.setFixedHeight(340)
     self.groupbox_frame_options.setFixedWidth(230)
-    self.groupbox_frame_options.setStyleSheet("QGroupBox::title {"
-                                              "padding-top:  8px;"
-                                              "padding-left: 8px;} ")
     self.groupbox_frame_options.move(630, 50)
 
-    # radio buttons settings
+    # radio buttons properties
     self.current_rb_selected = None
     self.button_group = QButtonGroup(self)
     self.rb_1 = QRadioButton('Bottom-left', self.groupbox_frame_options)
@@ -98,7 +107,7 @@ def load(self):
     self.button_group.addButton(self.rb_3, 3)
     self.button_group.addButton(self.rb_4, 4)
 
-    # checkbox settings
+    # checkbox properties
     self.cbox_enable_bbox = QCheckBox('Bounding Box', self.groupbox_frame_options)
     self.cbox_enable_bbox.move(10, 110)
     self.cbox_enable_class = QCheckBox('Class', self.groupbox_frame_options)
@@ -112,16 +121,19 @@ def load(self):
     self.cbox_enable_conf.setChecked(True)
     self.cbox_enable_info_background.setChecked(True)
 
-    # statusbar settings
+    # statusbar properties
     self.status_bar = QStatusBar()
+    self.status_bar.setSizeGripEnabled(False)
+    self.status_bar.setProperty("last_msg_time", QDateTime.currentDateTime().toSecsSinceEpoch())
+    self.status_bar.setStyleSheet('background-color: #3555a0;font-weight: bold;')
     self.setStatusBar(self.status_bar)
     self.label_class_info = QLabel("Class: -")
     self.status_bar.addPermanentWidget(self.label_class_info)
-    self.label_conf = QLabel("Confidence: -")
+    self.label_conf = QLabel("Confidence: 0.00")
     self.status_bar.addPermanentWidget(self.label_conf)
     self.label_dim = QLabel("Image size: -")
     self.status_bar.addPermanentWidget(self.label_dim)
-    self.label_fps = QLabel("FPS: -")
+    self.label_fps = QLabel("FPS: 0.00")
     self.status_bar.addPermanentWidget(self.label_fps)
     self.label_memory_usage = QLabel('Memory: -')
     self.status_bar.addPermanentWidget(self.label_memory_usage)
@@ -132,46 +144,46 @@ def load(self):
     # create timer for buttons
     self.timer_start = QTimer()
     self.timer_stop = QTimer()
+    self.timer_statusbar_idle = QTimer()
+    self.timer_statusbar_idle.timeout.connect(lambda: check_idle_time(self))
+    self.timer_statusbar_idle.start(2000)
 
-    self.btn_color_box = QPushButton('', self.groupbox_frame_options)
-    self.btn_color_box.move(140, 118)
-    self.btn_color_box.setFixedHeight(20)
-    self.btn_color_box.setFixedWidth(20)
-    self.btn_color_class = QPushButton('', self.groupbox_frame_options)
-    self.btn_color_class.move(140, 178)
-    self.btn_color_class.setFixedHeight(20)
-    self.btn_color_class.setFixedWidth(20)
-    self.btn_color_conf = QPushButton('', self.groupbox_frame_options)
-    self.btn_color_conf.move(140, 148)
-    self.btn_color_conf.setFixedHeight(20)
-    self.btn_color_conf.setFixedWidth(20)
-    self.btn_color_bg = QPushButton('', self.groupbox_frame_options)
-    self.btn_color_bg.move(140, 208)
-    self.btn_color_bg.setFixedHeight(20)
-    self.btn_color_bg.setFixedWidth(20)
-    self.btn_color_box.setStyleSheet(
-        f"background-color: rgb({self.box_color[0]}, {self.box_color[1]}, {self.box_color[2]})")
-    self.btn_color_class.setStyleSheet(
-        f"background-color: rgb({self.text_color_class[0]}, {self.text_color_class[1]}, {self.text_color_class[2]})")
-    self.btn_color_conf.setStyleSheet(
-        f"background-color: rgb({self.text_color_conf[0]}, {self.text_color_conf[1]}, {self.text_color_conf[2]})")
-    self.btn_color_bg.setStyleSheet(
-        f"background-color: rgb({self.text_color_bg[0]}, {self.text_color_bg[1]}, {self.text_color_bg[2]})")
+    self.button_color_box = QPushButton('', self.groupbox_frame_options)
+    self.button_color_box.move(140, 118)
+    self.button_color_box.setFixedHeight(20)
+    self.button_color_box.setFixedWidth(20)
+    self.button_color_class = QPushButton('', self.groupbox_frame_options)
+    self.button_color_class.move(140, 178)
+    self.button_color_class.setFixedHeight(20)
+    self.button_color_class.setFixedWidth(20)
+    self.button_color_confidence = QPushButton('', self.groupbox_frame_options)
+    self.button_color_confidence.move(140, 148)
+    self.button_color_confidence.setFixedHeight(20)
+    self.button_color_confidence.setFixedWidth(20)
+    self.button_color_bg = QPushButton('', self.groupbox_frame_options)
+    self.button_color_bg.move(140, 208)
+    self.button_color_bg.setFixedHeight(20)
+    self.button_color_bg.setFixedWidth(20)
+    self.button_color_box.setStyleSheet(
+        f"background-color: rgb({self.box_color[0]}, {self.box_color[1]}, {self.box_color[2]});border: none")
+    self.button_color_class.setStyleSheet(
+        f"background-color: rgb({self.text_color_class[0]}, {self.text_color_class[1]}, {self.text_color_class[2]});border: none")
+    self.button_color_confidence.setStyleSheet(
+        f"background-color: rgb({self.text_color_conf[0]}, {self.text_color_conf[1]}, {self.text_color_conf[2]});border: none")
+    self.button_color_bg.setStyleSheet(
+        f"background-color: rgb({self.text_color_bg[0]}, {self.text_color_bg[1]}, {self.text_color_bg[2]});border: none")
 
     self.groupbox_general_options = QGroupBox(self)
     self.groupbox_general_options.setTitle('General Options')
     self.groupbox_general_options.setFixedHeight(100)
     self.groupbox_general_options.setFixedWidth(230)
-    self.groupbox_general_options.setStyleSheet("QGroupBox::title {"
-                                                "padding-top:  8px;"
-                                                "padding-left: 8px;} ")
     self.groupbox_general_options.move(630, 400)
     self.cbox_enable_debug = QCheckBox('Debug Info', self.groupbox_general_options)
     self.cbox_enable_debug.move(10, 30)
     self.cbox_enable_debug.setChecked(True)
     self.cbox_enable_debug.stateChanged.connect(self.set_debug_mode)
 
-    # slider settings - brightness
+    # slider properties - brightness
     self.label_brightness_control = QLabel('Brightness:', self.groupbox_frame_options)
     self.label_brightness_control.move(10, 245)
     self.slider_brightness = QSlider(Qt.Horizontal, self.groupbox_frame_options)
@@ -182,18 +194,12 @@ def load(self):
     self.slider_brightness.setValue(100)
     self.slider_brightness.setSingleStep(1)
     self.slider_brightness.setTickInterval(10)
-    self.slider_brightness.setStyleSheet("QSlider::add-page:horizontal {"
-                                         "background: gray;"
-                                         "}"
-                                         "QSlider::sub-page:horizontal {"
-                                         " background: gray;"
-                                         "}")
     self.label_brightness_control = QLabel(str(self.slider_brightness.value()) + '%', self.groupbox_frame_options)
     self.label_brightness_control.move(115, 270)
     self.slider_brightness.valueChanged.connect(
         lambda: update_slider_text(self.slider_brightness, self.label_brightness_control))
 
-    # slider settings - contrast
+    # slider properties - contrast
     self.label_contrast_control = QLabel('Contrast:', self.groupbox_frame_options)
     self.label_contrast_control.move(10, 290)
     self.slider_contrast = QSlider(Qt.Horizontal, self.groupbox_frame_options)
@@ -204,12 +210,6 @@ def load(self):
     self.slider_contrast.setValue(100)
     self.slider_contrast.setSingleStep(1)
     self.slider_contrast.setTickInterval(10)
-    self.slider_contrast.setStyleSheet("QSlider::add-page:horizontal {"
-                                       "background: gray;"
-                                       "}"
-                                       "QSlider::sub-page:horizontal {"
-                                       " background: gray;"
-                                       "}")
     self.label_contrast_control = QLabel(str(self.slider_contrast.value()) + '%', self.groupbox_frame_options)
     self.label_contrast_control.move(115, 315)
     self.slider_contrast.valueChanged.connect(
@@ -217,14 +217,13 @@ def load(self):
 
     self.button_reset_brightness = QPushButton('Reset', self.groupbox_frame_options)
     self.button_reset_contrast = QPushButton('Reset', self.groupbox_frame_options)
+
     self.button_reset_brightness.setFixedWidth(68)
     self.button_reset_brightness.setFixedHeight(20)
     self.button_reset_contrast.setFixedWidth(68)
     self.button_reset_contrast.setFixedHeight(20)
     self.button_reset_contrast.move(150, 312)
-    self.button_reset_contrast.setStyleSheet("font-size: 10px;")
     self.button_reset_brightness.move(150, 267)
-    self.button_reset_brightness.setStyleSheet("font-size: 10px;")
 
     # start memory thread
     self.worker_thread_memory = WorkerThreadSystemResource()
@@ -242,10 +241,10 @@ def load(self):
     self.button_stop.clicked.connect(self.on_button_stop_clicked)
     self.timer_start.timeout.connect(lambda: timer_timeout_start(self))
     self.timer_stop.timeout.connect(lambda: timer_timeout_stop(self))
-    self.btn_color_box.clicked.connect(lambda: show_color_picker(self, self.btn_color_box))
-    self.btn_color_class.clicked.connect(lambda: show_color_picker(self, self.btn_color_class))
-    self.btn_color_conf.clicked.connect(lambda: show_color_picker(self, self.btn_color_conf))
-    self.btn_color_bg.clicked.connect(lambda: show_color_picker(self, self.btn_color_bg))
+    self.button_color_box.clicked.connect(lambda: show_color_picker(self, self.button_color_box))
+    self.button_color_class.clicked.connect(lambda: show_color_picker(self, self.button_color_class))
+    self.button_color_confidence.clicked.connect(lambda: show_color_picker(self, self.button_color_confidence))
+    self.button_color_bg.clicked.connect(lambda: show_color_picker(self, self.button_color_bg))
     self.button_reset_brightness.clicked.connect(lambda: self.slider_brightness.setValue(100))
     self.button_reset_contrast.clicked.connect(lambda: self.slider_contrast.setValue(100))
 
@@ -254,6 +253,27 @@ def load(self):
 
     # disable stop button on start
     self.button_stop.setEnabled(False)
+
+    # set all groupboxes to specific color
+    for groupBox in self.findChildren(QGroupBox):
+        groupBox.setStyleSheet('QGroupBox {background-color: #323844;font-weight: bold;}')
+
+    for groupBox in self.findChildren(QGroupBox):
+        for widget in groupBox.findChildren(QWidget):
+            if not isinstance(widget, QPushButton):
+                widget.setStyleSheet("background-color: #323844;font-weight: bold;")
+    self.button_reset_brightness.setStyleSheet('QPushButton {'
+                                               'font-size: 10px;}'
+                                               'QPushButton:enabled {'
+                                               'background-color: #4269b9;'
+                                               'border: 1px solid white;}')
+    self.button_reset_contrast.setStyleSheet('QPushButton {'
+                                             'font-size: 10px;}'
+                                             'QPushButton:enabled {'
+                                             'background-color: #4269b9;'
+                                             'border: 1px solid white;}')
+    #self.button_color_box.setStyleSheet('border: 1px solid black')
+
 
 
 # on timeout stop button
@@ -299,21 +319,22 @@ def show_color_picker(self, button):
 
     Args:
         button: The button which the color is applied.
+        self:
     """
     color = QColorDialog.getColor()
     if color.isValid():
         # update the color tuple with the new RGB values
         color_tuple = (color.red(), color.green(), color.blue())
         # set the background color of the button using stylesheet
-        button.setStyleSheet(f"background-color: rgb({color.red()}, {color.green()}, {color.blue()})")
+        button.setStyleSheet(f"background-color: rgb({color.red()}, {color.green()}, {color.blue()});border: none")
         # update the original color tuple with the new RGB values
-        if button == self.btn_color_box:
+        if button == self.button_color_box:
             self.box_color = color_tuple
-        elif button == self.btn_color_class:
+        elif button == self.button_color_class:
             self.text_color_class = color_tuple
-        elif button == self.btn_color_conf:
+        elif button == self.button_color_confidence:
             self.text_color_conf = color_tuple
-        elif button == self.btn_color_bg:
+        elif button == self.button_color_bg:
             self.text_color_bg = color_tuple
 
 
@@ -326,3 +347,16 @@ def update_slider_text(control, label):
         label: the label to be updated.
     """
     label.setText(str(control.value()) + '%')
+
+
+def check_idle_time(self):
+    if self.status_bar.currentMessage() == "Idle":
+        return
+
+    last_msg_time = self.status_bar.property("last_msg_time")
+    if last_msg_time is None:
+        return
+
+    elapsed_time = QDateTime.currentDateTime().toSecsSinceEpoch() - last_msg_time
+    if elapsed_time >= 1 & (not self.status_bar.currentMessage() == "Getting camera stream.."):
+        self.status_bar.showMessage("Idle")
