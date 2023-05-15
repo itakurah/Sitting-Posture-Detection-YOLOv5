d8361ff413443c76bfd1b053c375fe60a2e080cf
itakurah <122729995+itakurah@users.noreply.github.com>
2023-05-15 15:08:22 +0200

    -added: added fullscreen helper class

---
+++ util/helper/fullscreen_helper.py
@@ -0,0 +1,24 @@
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel, QMainWindow
+
+
+class FullscreenWindow(QMainWindow):
+    fullscreen_closed = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel()
+        self.label.setAlignment(Qt.AlignCenter)
+        self.setWindowState(Qt.WindowFullScreen)
+
+    def set_central_widget_content(self, pixmap):
+        if pixmap is not None:
+            pixmap_scaled = pixmap.scaled(self.size(), Qt.AspectRatioMode.KeepAspectRatio)
+            self.label.setPixmap(QPixmap.fromImage(pixmap_scaled))
+        self.setCentralWidget(self.label)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Escape or event.key() == Qt.Key_Q:
+            self.close()
+            self.fullscreen_closed.emit()
