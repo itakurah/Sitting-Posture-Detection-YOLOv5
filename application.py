b70bb6c1671d61bede7d0755dd893ce4e71861bb
itakurah <122729995+itakurah@users.noreply.github.com>
2023-05-30 15:56:14 +0200

    refactor: removed class

--- application.py
+++ application.py
@@ -2,77 +2,50 @@ import os
 import sys
 import tempfile
 
-import cv2
-import psutil
-from PyQt5 import QtCore
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QImage, QColor, QPainter
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication
+from qt_material import apply_stylesheet
+
+from app_controllers.controller import Controller
+from app_models.model import Model
+from app_views.view import View
 
 
-from util.gui import gui, frame_properties
-
-from util.threads.worker_thread_pause_screen import WorkerThreadPauseScreen
-from views.view import View
-
-
-class Application(QMainWindow):
-    os.environ['CURL_CA_BUNDLE'] = ''
-
+class App:
     def __init__(self):
         super().__init__()
-        #self.view = View()
-        # annotate class variables
+        self.model = Model(model_name)
+        self.view = View(self.model)
+        self.controller = Controller(self.model, self.view)
+        print("All modules loaded")
 
 
-        # load frame properties
-        #frame_properties.load(self)
-        # load gui
-        #gui.load(self)
-
-
-
-
-
-
-    # on click start button
-
-
-    # on click stop button
-
-
-
-
-
-
-
-
-
-
-
-    # update pixmap with when no camera is available
-
-
-
-
-    # stop pause screen worker thread
-
-
-    # initialize worker thread for camera capture
-
-
-    def closeEvent(self, event):
-        cv2.destroyAllWindows()
-        self.stop_worker_thread_camera()
-        self.worker_thread_memory.stop()
-
-
-def get_model_name():
-    return model_name
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        print("Error: Exactly one argument is expected")
+        sys.exit(1)
+    elif len(sys.argv) == 1:
+        print("Info: Loading default inference_models")
+        model_name = "model_s_best.pt"
+    else:
+        model_name = sys.argv[1]
+    style = '''<!--?xml version="1.0" encoding="UTF-8"?-->
+    <resources>
+      <color name="primaryColor">#ffffff</color>
+      <color name="primaryLightColor">#6e6d6d</color>
+      <color name="secondaryColor">#323844</color>
+      <color name="secondaryLightColor">#4f5b62</color>
+      <color name="secondaryDarkColor">#31363b</color>
+      <color name="primaryTextColor">#ffffff</color>
+      <color name="secondaryTextColor">#ffffff</color>
+    </resources>'''
+    app = QApplication([])
+    window = App()
+    with tempfile.NamedTemporaryFile(suffix='.xml', delete=False) as tmp:
+        # Write the XML string to the temporary file
+        tmp.write(style.encode('utf-8'))
+        # Close the temporary file
+        tmp.close()
+        apply_stylesheet(app, theme=tmp.name, css_file='app_views/custom.css')
+    os.unlink(tmp.name)
+    window.view.show()
+    sys.exit(app.exec())
