dfe94dbaaa892433860a8c8a186b64e502eb3dce
itakurah <122729995+itakurah@users.noreply.github.com>
2024-11-19 16:27:47 +0100

    optimized pause screen calculation

--- app_views/threads/worker_thread_pause_screen.py
+++ app_views/threads/worker_thread_pause_screen.py
@@ -11,7 +11,6 @@ class WorkerThreadPauseScreen(QtCore.QThread):
     update_pause_screen = QtCore.pyqtSignal(object, QPixmap)
 
     def __init__(self, view, width, height):
-        # Use super() to call __init__() methods in the parent classes
         super(WorkerThreadPauseScreen, self).__init__()
         self.view = view
         self.pixmap = QPixmap(width, height)
@@ -22,22 +21,26 @@ class WorkerThreadPauseScreen(QtCore.QThread):
         self.running = True
 
     def run(self):
+        scale_factor = 2
+        small_width = self.width // scale_factor
+        small_height = self.height // scale_factor
+
         while self.running:
-            # generate random RGB colors for all pixels in one step
-            colors = np.random.rand(self.height, self.width, 3) * 255
-            colors = colors.astype(np.uint8)
+            # Generate random RGB colors for scaled-down dimensions
+            colors = np.random.randint(0, 256, (small_height, small_width, 3), dtype=np.uint8)
 
-            # group neighboring pixels to create larger pixels
-            scale_factor = 2  # Adjust the scale factor to control the size of the grouped pixels
-            resized_colors = cv2.resize(colors, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_NEAREST)
+            # Upscale by repeating the values
+            scaled_colors = np.repeat(np.repeat(colors, scale_factor, axis=0), scale_factor, axis=1)
 
-            # create a QImage from the NumPy array
-            image = QImage(resized_colors.data, resized_colors.shape[1], resized_colors.shape[0], QImage.Format_RGB888)
+            # Ensure dimensions match exactly
+            scaled_colors = scaled_colors[:self.height, :self.width]
+
+            # Create a QImage directly from the scaled NumPy array
+            image = QImage(scaled_colors.data, scaled_colors.shape[1], scaled_colors.shape[0], QImage.Format_RGB888)
 
             self.pixmap = QPixmap.fromImage(image)
             self.update_pause_screen.emit(self.view, self.pixmap)
-            self.msleep(90)
+            self.msleep(100)
 
     def stop(self):
-        # terminate the while loop in self.run() method
         self.running = False
